211c8da647e9060339b7af7e9cb7c887e66a5cd0
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2020-11-02 12:09:41 -0500

    commit tree 6b91120166f2d8735ddf6989e919fb66ec85ad66

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,71 @@
-# your code goes here
+require 'pry'
+
+class Person
+  attr_reader :name, :hygiene, :happiness
+
+  attr_accessor :bank_account
+
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+
+  def happiness=(happiness)
+    @happiness = happiness
+    @happiness = 10 if happiness > 10
+    @happiness = 0 if happiness < 0
+  end
+
+  def hygiene=(hygiene)
+    @hygiene = hygiene
+    @hygiene = 10 if @hygiene > 10
+    @hygiene = 0 if hygiene < 0
+  end
+
+  def happy?
+    @happiness > 7
+  end
+
+  def clean?
+    @hygiene > 7
+  end
+
+  def get_paid(salary)
+    @bank_account += salary
+    'all about the benjamins'
+  end
+
+  def take_bath
+    self.hygiene = @hygiene + 4
+    '♪ Rub-a-dub just relaxing in the tub ♫'
+  end
+
+  def work_out
+    self.hygiene = @hygiene - 3
+    self.happiness = @happiness + 2
+    '♪ another one bites the dust ♫'
+  end
+
+  def call_friend(friend)
+    self.happiness = @happiness + 3
+    friend.itself.happiness = friend.happiness + 3
+    "Hi #{friend.name}! It's #{name}. How are you?"
+  end
+
+  def start_conversation(person, topic)
+    case topic
+    when 'politics'
+      self.happiness = @happiness - 2
+      person.itself.happiness = person.happiness - 2
+      'blah blah partisan blah lobbyist'
+    when 'weather'
+      self.happiness = @happiness + 1
+      person.itself.happiness = person.happiness + 1
+      'blah blah sun blah rain'
+    else
+      'blah blah blah blah blah'
+    end
+  end
+end
